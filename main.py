d8bae02c47cad44576aa4ed4cab253f40f932559
sainikmandal <68194201+sainik-create@users.noreply.github.com>
2024-10-20 07:14:21 +0530

    Update main.py to set the fps to 60

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+        
+    # Setting fps to 60
+    clock = pygame.time.Clock()
+    dt = 0
+    
     # Game loop
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+                
+        dt = clock.tick(60) / 1000  # Update dt each frame
+
         # Fill the screen with black
         screen.fill((0, 0, 0))
 
         # Update the display
         pygame.display.flip()
 
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
-
