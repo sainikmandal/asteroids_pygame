d581f1cda8b86143090a431181f9929bba8a5b37
sainikmandal <68194201+sainik-create@users.noreply.github.com>
2024-10-20 23:12:08 +0530

    Impliment shooting

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import pygame
+import sys
 from constants import *
-from player import Player  # Ensure this import is correctly placed
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))        
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
     # Setting fps to 60
     clock = pygame.time.Clock()
     # Create a Player object in the middle of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20)  # 20 is the radius
+     # Create groups for updatable and drawable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    # Add the player to both groups
+    updatable.add(player)
+    drawable.add(player)   
     # Game loop
     running = True
     while running:
@@ -19,14 +38,26 @@ def main():
                 
         dt = clock.tick(60) / 1000  # Update dt each frame
 
-        # Update the player, passing the dt
-        player.update(dt)
+        # Update all objects in the 'updatable' group
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
 
         # Fill the screen with black
         screen.fill((0, 0, 0))
 
-        # Draw the player
-        player.draw(screen)  # Render the player on the screen
+        for obj in drawable:
+            obj.draw(screen)
 
         # Update the display
         pygame.display.flip()
